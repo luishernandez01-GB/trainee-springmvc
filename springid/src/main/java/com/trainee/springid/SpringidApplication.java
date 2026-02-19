2eccb70fc1b97febae0998d47543a8203e932511
luishernandez01-GB <luis.hernandez01@globant.com>
2026-02-19 09:09:39 -0500

    Add initial Spring MVC project structure with Maven wrapper, configuration files, and application setup

---
+++ springid/src/main/java/com/trainee/springid/SpringidApplication.java
@@ -0,0 +1,13 @@
+package com.trainee.springid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringidApplication.class, args);
+	}
+
+}
---
+++ springid/src/test/java/com/trainee/springid/SpringidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainee.springid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
